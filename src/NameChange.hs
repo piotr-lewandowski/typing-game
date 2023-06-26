4830ba5ca3f1395a02a39023773838da1438d477
Piotr Lewandowski <lewandowskipiotr98@gmail.com>
2023-06-26 16:08:22 +0200

    Limit names to 10 characters

--- src/NameChange.hs
+++ src/NameChange.hs
@@ -12,8 +12,16 @@ data NameChangeState = NameChangeState {_currentName :: Name, _newName :: Name} 
 makeLenses ''NameChangeState
 
 handleNameChangeInput :: InputEvents -> NameChangeState -> Either Name NameChangeState
-handleNameChangeInput (Typed c) nameChange = Right $ nameChange & newName <>~ [c]
+handleNameChangeInput (Typed c) nameChange =
+    Right $
+        if length (nameChange ^. newName) < 10
+            then nameChange & newName <>~ [c]
+            else nameChange
 handleNameChangeInput Backspace nameChange = Right $ nameChange & newName %~ init
-handleNameChangeInput Confirm nameChange = Left $ nameChange ^. newName
+handleNameChangeInput Confirm nameChange =
+    Left $
+        if null (nameChange ^. newName)
+            then nameChange ^. currentName
+            else nameChange ^. newName
 handleNameChangeInput Exit nameChange = Left $ nameChange ^. currentName
 handleNameChangeInput _ nameChange = Right nameChange
